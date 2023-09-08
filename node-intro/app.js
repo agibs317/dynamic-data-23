5a3c54f6321e384d522c1d2668cd50285e9be831
agibs317 <andaiye.business@gmail.com>
2023-09-07 20:30:34 -0400

    First class experience

---
+++ node-intro/app.js
@@ -0,0 +1,41 @@
+var name = "Andaiye Gibson";
+
+var myNumber = 13;
+
+const port = 3000;
+// Objects
+var car = {
+    make: "Audi",
+    model: "S5",
+    year: 2023
+}
+//functions or methods that returns a value
+function addTwo(somenumber) {
+    return somenumber + 2;
+}
+
+//functions that perform a task
+function outputSomething(output){
+    console.log("You wrote: " + output)
+}
+
+//outputSomething("Write something here... I'm (kind of) having fun with node");
+
+//var result = addTwo(10);
+//outputSomething(result);
+//outputSomething(addTwo(10));
+
+//console.log("Hello world!!");
+
+//console.log(car);
+
+//access object properties with "."
+//outputSomething(car.model);
+
+//console.log( "My name is " + name + " and my favorite number is " + myNumber );
+console.log(name + myNumber);
+console.log(myNumber + myNumber);
+console.log(myNumber + "22");
+
+
+
